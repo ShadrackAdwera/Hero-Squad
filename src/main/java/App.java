2fd10464a57e81a34a5c4a1bd226d776e9c21552
ADWESH <adweshshaddie@gmail.com>
2020-01-04 12:08:38 +0300

    make it better

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,15 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     public static void main(String[]args){
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
         //get: show all heroes
         get("/", (request, response) -> {
